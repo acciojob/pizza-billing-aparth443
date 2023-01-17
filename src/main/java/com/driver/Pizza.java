bd43a1e66f370f1279c9d5ee2ed956bcd5337f46
Parth Agrawal <aparth443@gmail.com>
2023-01-17 17:44:22 +0530

    first try

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,25 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private Boolean extraCheese;
+    private Boolean extraToppings;
+    private Boolean isTakeAway;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg == true){
+            this.price = 300;
+            this.bill = "Base Price Of The Pizza: 300\n";
+        }else{
+            this.price = 400;
+            this.bill = "Base Price Of The Pizza: 400\n";
+        }
+        extraCheese = false;
+        extraToppings = false;
+        isTakeAway = false;
+
     }
 
     public int getPrice(){
@@ -17,18 +32,51 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(extraCheese == true){
+            return;
+        }
+        extraCheese = true;
+        this.price += 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(extraToppings == true){
+            return;
+        }
+        extraToppings = true;
+        if(isVeg == true){
+            this.price += 70;
+        }else{
+            this.price += 120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(isTakeAway == true){
+            return;
+        }
+        isTakeAway = true;
+        this.price += 20;
     }
 
     public String getBill(){
         // your code goes here
+        if(extraCheese == true){
+            bill += "Extra Cheese Added: 80\n";
+        }
+        if(extraToppings == true){
+            if(isVeg == true){
+                bill += "Extra Toppings Added: 70\n";
+            }else{
+                bill += "Extra Toppings Added: 120\n";
+            }
+        }
+        if(isTakeAway == true){
+            bill += "Paperbag Added: 20\n";
+        }
+        bill += "Total Price: " + this.price + "\n";
         return this.bill;
     }
 }
